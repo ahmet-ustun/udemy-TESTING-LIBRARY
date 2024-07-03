9b6423161dbeee26bba86c522512e1748a7fe018
Ahmet Ustun <austu@softserveinc.com>
2024-07-03 16:33:24 +0300

    chore: Add checkbox to disable button and update tests in App component

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -5,11 +5,15 @@ function App() {
   const [buttonColor, setButtonColor] = React.useState("red");
   const nextColor = buttonColor === "red" ? "blue" : "red";
 
+  const [isDisabled, setIsDisabled] = React.useState(false);
+
   return (
     <div>
       <button 
         className={buttonColor} 
-        onClick={() => setButtonColor(nextColor)}>
+        onClick={() => setButtonColor(nextColor)}
+        disabled={isDisabled}
+      >
           Change to {nextColor}
       </button>
       <br />
@@ -17,6 +21,8 @@ function App() {
         type="checkbox" 
         id="disable-button-checkbox" 
         defaultChecked={false} 
+        checked={isDisabled}
+        onChange={(e) => setIsDisabled(e.target.checked)}
       />
       <label htmlFor="disable-button-checkbox">
         Disable button
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -22,7 +22,7 @@ test("button has correct label and color after click", () => {
   expect(buttonElement).toHaveClass("blue");
 });
 
-test("checkbox flow", () => {
+test("checkbox flow 1", () => {
   render(<App />);
   
   const buttonElement = screen.getByRole("button", { name: /blue/i });
@@ -35,4 +35,24 @@ test("checkbox flow", () => {
   
   expect(checkboxElement).toBeChecked();
   expect(buttonElement).toBeDisabled();
+});
+
+test("checkbox flow 2", () => {
+  render(<App />);
+  
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", { name: /disable button/i });
+  
+  expect(checkboxElement).not.toBeChecked();
+  expect(buttonElement).toBeEnabled();
+  
+  fireEvent.click(checkboxElement);
+  
+  expect(checkboxElement).toBeChecked();
+  expect(buttonElement).toBeDisabled();
+
+  fireEvent.click(checkboxElement);
+
+  expect(checkboxElement).not.toBeChecked();
+  expect(buttonElement).toBeEnabled();
 });
