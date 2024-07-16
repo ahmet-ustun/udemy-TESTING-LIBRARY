54f9e186f7b6ca597cafd1518811e797ddc5e968
Ahmet Ustun <austu@softserveinc.com>
2024-07-16 11:38:46 +0300

    feat: Update Options.test.jsx to use async/await for fetching scoop images
    This commit updates the Options.test.jsx file to use async/await when fetching scoop images from the server. By using async/await, we ensure that the test waits for the images to be loaded before making assertions. This improves the reliability of the test and ensures that the correct number of images are displayed for each scoop option.

--- sundae-starter/src/pages/entry/tests/Options.test.jsx
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
-test("Displays image for each scoop option from server", () => {
+test("Displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   const altTexts = scoopImages.map((element) => element.alt);
