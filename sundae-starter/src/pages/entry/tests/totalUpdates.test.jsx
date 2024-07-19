72703313209d1cb18a1c34f94ed98da7addca59a
Ahmet Ustun <austu@softserveinc.com>
2024-07-19 19:46:50 +0300

    feat: Add test for updating scoop subtotal in totalUpdates.test.jsx

---
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import Options from "../Options.jsx";
+import userEvent from "@testing-library/user-event";
+
+test("Update scoop subtotal when scoops change", async () => {
+  const user = userEvent.setup();
+
+  render(<Options optionType="scoops" />);
+
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
