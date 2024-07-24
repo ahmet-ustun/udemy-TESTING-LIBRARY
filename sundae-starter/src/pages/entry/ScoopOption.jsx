287ff4b8424f4a2cae811ad0396b729e0e66ec19
Ahmet Ustun <austu@softserveinc.com>
2024-07-24 18:11:50 +0300

    feat: Add form control for scoop count in ScoopOption component

--- sundae-starter/src/pages/entry/ScoopOption.jsx
+++ sundae-starter/src/pages/entry/ScoopOption.jsx
@@ -1,6 +1,15 @@
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
 
 export default function ScoopOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+
+  const handleChange = (e) => {
+    updateItemCount(name, parseInt(e.target.value), "scoops");
+  };
+
   return (
     <Col
       xs={12}
@@ -16,6 +25,22 @@ export default function ScoopOption({ name, imagePath }) {
         alt={`${name} scoop`}
         style={{ width: "75%" }}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: "left" }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
