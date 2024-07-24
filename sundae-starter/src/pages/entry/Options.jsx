fca548fa9de371f6c36969ec712037c952909da6
Ahmet Ustun <austu@softserveinc.com>
2024-07-24 18:11:40 +0300

    feat: Add title and pricing information to Options component

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -4,10 +4,15 @@ import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
 import AlertBanner from "../common/AlertBanner.jsx";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+
+  const { totals } = useOrderDetails();
 
   useEffect(() => {
     axios
@@ -24,6 +29,8 @@ export default function Options({ optionType }) {
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
@@ -32,5 +39,14 @@ export default function Options({ optionType }) {
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals)}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
