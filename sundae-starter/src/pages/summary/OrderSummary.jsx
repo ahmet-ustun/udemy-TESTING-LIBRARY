296803a460d507136088ad70341688e8c2325fbd
Ahmet Ustun <austu@softserveinc.com>
2024-07-24 18:11:59 +0300

    feat: Update OrderSummary component to display order details and summary

--- sundae-starter/src/pages/summary/OrderSummary.jsx
+++ sundae-starter/src/pages/summary/OrderSummary.jsx
@@ -1,3 +1,26 @@
+import SummaryForm from "./SummaryForm.jsx";
+import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
+import { formatCurrency } from "../../utilities";
+
 export default function OrderSummary() {
-  return <div>Test</div>;
+  const { totals, optionCounts } = useOrderDetails();
+
+  const scoopsArray = Object.entries(optionCounts.scoops);
+  const scoopsList = scoopsArray.map(([key, value]) => (
+    <li key={key}>{`${value} ${key}`}</li>
+  ));
+
+  const toppingsArray = Object.keys(optionCounts.toppings);
+  const toppingsList = toppingsArray.map((key) => <li key={key}>{key}</li>);
+
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopsList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <ul>{toppingsList}</ul>
+      <SummaryForm />
+    </div>
+  );
 }
