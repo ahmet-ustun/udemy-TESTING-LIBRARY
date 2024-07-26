d00cc749fa6571980b41a92978aa6358c09e3335
Ahmet Ustun <austu@softserveinc.com>
2024-07-26 18:37:59 +0300

    feat: Add grand total to OrderEntry component

--- sundae-starter/src/pages/entry/OrderEntry.jsx
+++ sundae-starter/src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,17 @@
+import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
+import { formatCurrency } from "../../utilities/index.js";
 import Options from "./Options.jsx";
 
 export default function OrderEntry() {
+  const {totals} = useOrderDetails()
+
+  const grandTotal = totals.scoops + totals.toppings
+
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {formatCurrency(grandTotal)}</h2>
     </div>
   );
 }
