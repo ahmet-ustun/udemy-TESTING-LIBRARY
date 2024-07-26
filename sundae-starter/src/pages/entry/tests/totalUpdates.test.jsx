42999d6559220a8709612df97b6ce39bf7330cf9
Ahmet Ustun <austu@softserveinc.com>
2024-07-26 16:48:05 +0300

    test: Update totalUpdates.test.jsx to include toppings subtotal calculation

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -29,3 +29,32 @@ test("Update scoop subtotal when scoops change", async () => {
 
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("Update toppings subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+
+  await user.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  await user.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+});
