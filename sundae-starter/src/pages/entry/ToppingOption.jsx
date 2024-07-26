7507cbe65dde733a8dacb3fbbf6baca8d199e028
Ahmet Ustun <austu@softserveinc.com>
2024-07-26 16:48:14 +0300

    feat: Add checkbox for toppings in ToppingOption component

--- sundae-starter/src/pages/entry/ToppingOption.jsx
+++ sundae-starter/src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,15 @@
+import { Form } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
+import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
 
 export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+
+  const handleChange = (e) => {
+    const value = e.target.checked ? 1 : 0
+    updateItemCount(name, value, "toppings");
+  };
+
   return (
     <Col
       xs={12}
@@ -16,6 +25,9 @@ export default function ToppingOption({ name, imagePath }) {
         alt={`${name} topping`}
         style={{ width: "75%" }}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" label={name} onChange={handleChange} />
+      </Form.Group>
     </Col>
   );
 }
