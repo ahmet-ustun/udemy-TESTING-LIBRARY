59e8e37ba05a33335778389cc9945ac79529aea4
Ahmet Ustun <austu@softserveinc.com>
2024-07-30 11:31:38 +0300

    feat: Enable/disable "Order sundae" button based on whether scoops are selected

--- sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,7 +1,12 @@
-import { render, screen, logRoles } from "../../../test-utils/testing-library-utils.jsx";
+import {
+  render,
+  screen,
+  logRoles,
+} from "../../../test-utils/testing-library-utils.jsx";
 import OrderEntry from "../OrderEntry";
 import { server } from "../../../mocks/server";
 import { http, HttpResponse } from "msw";
+import userEvent from "@testing-library/user-event";
 
 test("Handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
@@ -28,9 +33,23 @@ test("my test 2", () => {});
 test("my test 3", () => {});
 
 test("Order button is disabled if there are no scoops ordered", async () => {
-  render(<OrderEntry />);
+  const user = userEvent.setup();
+
+  render(<OrderEntry setOrderPhase={vi.fn()} />);
 
   const orderButton = screen.getByRole("button", { name: /order sundae/i });
 
   expect(orderButton).toBeDisabled();
-})
+
+  const vanillaInput  = await screen.findByRole("spinbutton", { name: "Vanilla" })
+
+  await user.clear(vanillaInput)
+  await user.type(vanillaInput, "1")
+
+  expect(orderButton).toBeEnabled();
+
+  await user.clear(vanillaInput)
+  await user.type(vanillaInput, "0")
+
+  expect(orderButton).toBeDisabled();
+});
