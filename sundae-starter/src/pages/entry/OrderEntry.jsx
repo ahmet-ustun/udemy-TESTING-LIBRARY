db65cb314322a942c5d4a5685caf58f4755636f5
Ahmet Ustun <austu@softserveinc.com>
2024-07-30 10:16:51 +0300

    feat: Add "Order sundae" button to OrderEntry component

--- sundae-starter/src/pages/entry/OrderEntry.jsx
+++ sundae-starter/src/pages/entry/OrderEntry.jsx
@@ -1,17 +1,23 @@
 import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
 import { formatCurrency } from "../../utilities/index.js";
 import Options from "./Options.jsx";
+import Button from "react-bootstrap/Button";
 
-export default function OrderEntry() {
-  const {totals} = useOrderDetails()
+export default function OrderEntry({ setOrderPhase }) {
+  const { totals } = useOrderDetails();
 
-  const grandTotal = totals.scoops + totals.toppings
+  const grandTotal = totals.scoops + totals.toppings;
+
+  const handleClick = () => {
+    setOrderPhase("review");
+  };
 
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {formatCurrency(grandTotal)}</h2>
+      <Button onClick={handleClick}>Order sundae</Button>
     </div>
   );
 }
