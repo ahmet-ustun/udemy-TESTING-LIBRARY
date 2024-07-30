6b88f07a6384e11d83a0293762ea497b651f93d1
Ahmet Ustun <austu@softserveinc.com>
2024-07-30 10:15:30 +0300

    feat: Add conditional component rendering based on order phase

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,12 +1,23 @@
 import Container from "react-bootstrap/Container";
 import OrderEntry from "./pages/entry/OrderEntry.jsx";
 import { OrderDetailsProvider } from "./contexts/OrderDetails.jsx";
+import OrderSummary from "./pages/summary/OrderSummary.jsx";
+import OrderConfirmation from "./pages/confirm/OrderConfirmation.jsx";
+import { useState } from "react";
 
 function App() {
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+
+  const OrderEntryComponent = {
+    inProgress: <OrderEntry setOrderPhase={setOrderPhase} />,
+    review: <OrderSummary setOrderPhase={setOrderPhase} />,
+    completed: <OrderConfirmation setOrderPhase={setOrderPhase} />,
+  };
+
   return (
     <Container>
       <OrderDetailsProvider>
-        <OrderEntry />
+        {OrderEntryComponent[orderPhase]}
       </OrderDetailsProvider>
     </Container>
   );
